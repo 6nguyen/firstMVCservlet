e8a5a38371dd4acbdd08e9ed92b4abcec4cb9b47
George Nguyen <g.nguyen92@yahoo.com>
2017-07-07 11:18:18 -0700

    Set up sample data; String[] students

---
+++ src/com/nguyen92/servlet/firstMVCservlet/MVCservlet.java
@@ -0,0 +1,50 @@
+package com.nguyen92.servlet.firstMVCservlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MVCservlet
+ */
+@WebServlet("/MVCservlet")
+public class MVCservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MVCservlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// Step 0:  Add sample data
+		String[] students = {"Ashley", "Megan", "Chloe"};
+		request.setAttribute("studentList", students);
+		
+		// Step 1:  get request dispatcher to send data to JSP
+		
+		
+		// Step 2:  forward the request to JSP file
+		
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
